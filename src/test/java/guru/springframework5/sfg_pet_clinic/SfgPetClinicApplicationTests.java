f14a5190d22b7eaf6c17060d50acb5ab715fd763
Your Name <prernark@gmail.com>
2020-12-04 16:51:32 +0000

    Initial Project Add

---
+++ src/test/java/guru/springframework5/sfg_pet_clinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework5.sfg_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
