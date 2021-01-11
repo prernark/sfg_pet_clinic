29a2a26b6629290a88998f9ce372bd9076d3749b
Your Name <prernark@gmail.com>
2021-01-11 16:58:44 +0000

    Adding Index Page and Controller. Closes #8

---
+++ pet_clinic_web/src/main/java/guru/springframework5/sfg_pet_clinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework5.sfg_pet_clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index"; //this will search for index.html in templates
+    }
+}
