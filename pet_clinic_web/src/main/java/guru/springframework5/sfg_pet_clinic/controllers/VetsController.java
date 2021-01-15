9fc8191f03f3d78e0af24ae88d532b9a2b42a07c
Your Name <prernark@gmail.com>
2021-01-15 16:43:23 +0000

    Added Vets List - so updated the Controller and the vet index.html. Closes #15

--- pet_clinic_web/src/main/java/guru/springframework5/sfg_pet_clinic/controllers/VetsController.java
+++ pet_clinic_web/src/main/java/guru/springframework5/sfg_pet_clinic/controllers/VetsController.java
@@ -1,13 +1,21 @@
 package guru.springframework5.sfg_pet_clinic.controllers;
 
+import guru.springframework5.sfg_pet_clinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listOfVets() {
+    public String listOfVets(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "Vets/index"; //This string corresponds to index.html in the Vets dir of templates
     }
 }
