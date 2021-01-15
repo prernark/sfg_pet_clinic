040deabc51e8415791426922fc77e5603d140b17
Your Name <prernark@gmail.com>
2021-01-15 16:11:42 +0000

    Added Owners List - so updated the Controller and the owner index.html. Closes #16

--- pet_clinic_web/src/main/java/guru/springframework5/sfg_pet_clinic/controllers/OwnerController.java
+++ pet_clinic_web/src/main/java/guru/springframework5/sfg_pet_clinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package guru.springframework5.sfg_pet_clinic.controllers;
 
+import guru.springframework5.sfg_pet_clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping ("/owner") //Instead of typing owner down several times we can say it once here at Class level
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
 
-//    @RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    //    @RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
     @RequestMapping({"", "/", "/index", "/index.html"}) //Dont need above coz of RequestMapping above
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "Owners/index";//This string corresponds to index.html in the Owner dir of templates
     }
 }
