bce7658efc41dd305321346e6488e03327afb6ce
Your Name <prernark@gmail.com>
2021-01-20 17:01:54 +0000

    Added contact detail properties for Owner. Closes #28

--- pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/Owner.java
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/Owner.java
@@ -3,7 +3,34 @@ package guru.springframework5.sfg_pet_clinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> petSet;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPetSet() {
         return petSet;
