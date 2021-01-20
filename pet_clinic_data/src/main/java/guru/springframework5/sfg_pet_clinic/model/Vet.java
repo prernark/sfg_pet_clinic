37aed48ba23ea62107813292d001b9ffdaa83893
Your Name <prernark@gmail.com>
2021-01-20 16:57:50 +0000

    Added the Speciality Entity and associated it to Vet. CLoses #27

--- pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/Vet.java
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/Vet.java
@@ -1,4 +1,15 @@
 package guru.springframework5.sfg_pet_clinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialitySet;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
 }
