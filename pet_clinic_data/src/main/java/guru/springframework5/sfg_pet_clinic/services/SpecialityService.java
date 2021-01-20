fef1569112e302efbfdcf0f9c3147bf9ffbca01c
Your Name <prernark@gmail.com>
2021-01-20 19:24:31 +0000

    Added Speciality Service and its Map implementation. Closes #31

---
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework5.sfg_pet_clinic.services;
+
+import guru.springframework5.sfg_pet_clinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/services/map/SpecialityServiceMapImpl.java
@@ -0,0 +1,10 @@
+package guru.springframework5.sfg_pet_clinic.services.map;
+
+import guru.springframework5.sfg_pet_clinic.model.Speciality;
+import guru.springframework5.sfg_pet_clinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityServiceMapImpl extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+}
