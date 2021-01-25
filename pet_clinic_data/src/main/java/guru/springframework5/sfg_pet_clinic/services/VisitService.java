58bb10a4077f7aa75d7519afcc2323650f90802e
Your Name <prernark@gmail.com>
2021-01-25 18:37:40 +0000

    Added Map implementation Service for Visit. Closes #46

---
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework5.sfg_pet_clinic.services;
+
+import guru.springframework5.sfg_pet_clinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework5.sfg_pet_clinic.services.map;
+
+import guru.springframework5.sfg_pet_clinic.model.Visit;
+import guru.springframework5.sfg_pet_clinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceMapImpl extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+            visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+}
