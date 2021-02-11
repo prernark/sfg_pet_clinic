d1b185bfecc433fd508dba4300e4f9ada75ff399
Your Name <prernark@gmail.com>
2021-02-11 16:34:00 +0000

    Fix displaying PetType in Owner Details. #62

--- pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/PetType.java
+++ pet_clinic_data/src/main/java/guru/springframework5/sfg_pet_clinic/model/PetType.java
@@ -17,7 +17,12 @@ public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-//    public String getName() {
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    //    public String getName() {
 //        return name;
 //    }
 //
